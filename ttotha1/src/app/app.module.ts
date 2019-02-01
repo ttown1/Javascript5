74a2a5d0a6bf60b506cb696600f1c3f9cab49d3b
ttown1 <37172558+ttown1@users.noreply.github.com>
2019-02-01 13:31:54 -0500

    Assignment 2
    Assignment 2

---
+++ ttotha1/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ContentCardComponent } from './content-card/content-card.component';
+import { ContentListComponent } from './content-list/content-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContentCardComponent,
+    ContentListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ttotha1/src/app/content-card/content-card-helper.ts
@@ -0,0 +1,8 @@
+export interface Content {
+    contentId: number;
+    author: string;
+    imgUrl?: string;
+    type?: string;
+    body: string;
+    tags?: string[];
+}
